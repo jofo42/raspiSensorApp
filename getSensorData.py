85f238f7cc06a5a3273ca3f1e8010d6339e1891b
Joachim Fohringer <joachim@joachimfohringer.de>
2013-05-26 16:34:22 +0200

    temporary disabled Arduino-communication.

--- getSensorData.py
+++ getSensorData.py
@@ -5,10 +5,11 @@ import sqlite3 as lite
 import sys
 import serial
 import re
+import subprocess
 from time import sleep
 from datetime import datetime
 
-arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=2)
+#arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=9600, timeout=2)
 
 con = None
 
@@ -31,32 +32,32 @@ try:
             while True:
                 # Run the DHT program to get the humidity and temperature readings!
 
-                output = subprocess.check_output(["./Adafruit_DHT", "2302", "4"])
+                output = subprocess.check_output(["./Adafruit_DHT", "11", "4"])
                 print output
                 matches = re.search("Temp =\s+([0-9.]+)", output)
                 if (not matches):
-                  time.sleep(3)
+                  sleep(3)
                   continue
                 temp = float(matches.group(1))
 
                 # search for humidity printout
                 matches = re.search("Hum =\s+([0-9.]+)", output)
                 if (not matches):
-                  time.sleep(3)
+                  sleep(3)
                   continue
                 humidity = float(matches.group(1))
 
                 print "Temperature: %.1f C" % temp
                 print "Humidity:    %.1f %%" % humidity
 
-            while True:
-                readings = arduino.readline().strip().split('\t')
-            	print readings
-                if len(readings) == 2:
-                    break
-            time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print "%s: t = %s C, h= %s %%" % (time, readings[1], readings[0])
-            sleep(60)
+#            while True:
+#                readings = arduino.readline().strip().split('\t')
+#            	print readings
+#                if len(readings) == 2:
+#                    break
+#            time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#            print "%s: t = %s C, h= %s %%" % (time, readings[1], readings[0])
+#            sleep(60)
             #cur.execute("INSERT INTO data(date, temp, hum) VALUES(?, ?, ?)", (time, readings[1], readings[0]))
             #con.commit()
 
